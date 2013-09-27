fb99b77c093cb6948238d3d63b493200515dc650
Muhammad Yaseen <mohammad_yaseen13@live.com>
2013-09-28 01:32:07 +0500

    Added two init funcs

--- mergetest.h
+++ mergetest.h
@@ -1,3 +1,17 @@
 /*
  * test file for merge conflicts
  */
+
+
+void RunLoo()
+{
+    for(int x=0; x < 100; x++)
+    {
+        //do nothing
+    }
+}
+
+int isEven(int x)
+{
+    return x % 2 == 0;
+}
