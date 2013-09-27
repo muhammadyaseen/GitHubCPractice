ab7f46c0d575faab684bc9fac1c93839dccfa8a2
Inshal <inshal240@hotmail.com>
2013-09-28 01:33:11 +0500

    New functions

--- mergetest.h
+++ mergetest.h
@@ -1,3 +1,16 @@
 /*
  * test file for merge conflicts
  */
+
+void printName(char *);
+float add(float, float);
+
+void printName(char* name)
+{
+    printf("\nThe given name is %s.\n", name);
+}
+
+float add(float x, float y)
+{
+    return (x+y);
+}
