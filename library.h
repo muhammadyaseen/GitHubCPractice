1f8b3e81fa95d5843583339b35cc6d74e7dcd12c
Muhammad Yaseen <mohammad_yaseen13@live.com>
2013-09-27 19:09:41 +0500

    Initial Commit to project

---
+++ library.h
@@ -0,0 +1,29 @@
+// PRE PROCESSOR DIRECTIVES
+
+
+// START FUNCTION PROTOTYEOPES
+
+
+// END FUNCTION PROTOTYPES
+
+//START FUNCTION DECLARATIONS
+
+
+/* Below is an example function declaration, please don't delete it and follow the same pattern for your functions.
+ * suggestions are welcome (variable and function name is irrelevant)
+ */
+
+/* @descripton - Brief description what the function is intended to do, or how it integrates into the larger project
+ * 
+ * @param myArg1 - A short message which makes purpose/use of myArg1 clear to reader
+ * @param myArg2 - A short message which makes purpose/use of myArg1 clear to reader
+ * @param myArg2 - A short message which makes purpose/use of myArg1 clear to reader
+ * 
+ * @returns - A message which describes what this function returns e.g 'Address of myArg2 in myArg3'
+ * @note - Any special note for example array bounds, limitations, cases where function fails should go here, brief and short
+ */
+void SomeAwesomeFunction(int myArg1, char myArg2, char * myArg3)
+{
+
+
+}
---
+++ main.c
@@ -0,0 +1,16 @@
+/* 
+ * This is the file where we 'call' all the implemented functions
+ * As such it should not have any function declaration or logic code.
+ * Function declaration and prototype should go in library.h file.
+ * Use this file to test your functions
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+    
+    return 0;
+    
+}
+
